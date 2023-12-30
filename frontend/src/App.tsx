54d973ada129139cc6e98abcd546fff30a179fc6
Li Feng Yin <lifeng-yin@proton.me>
2023-12-30 14:31:35 -0500

    :sparkles: Add connection and keydown logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css'
+import { useState, useEffect } from 'react';
 import Pusher from 'pusher-js';
 import { PusherProvider } from './contexts/PusherContext';
-import { usePusher } from './hooks/usePusher';
+import './App.css'
 
 const pusher = new Pusher(import.meta.env.VITE_PUSHER_KEY, {
   cluster: import.meta.env.VITE_PUSHER_CLUSTER
@@ -9,9 +9,32 @@ const pusher = new Pusher(import.meta.env.VITE_PUSHER_KEY, {
 Pusher.logToConsole = import.meta.env.DEV
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (!isLoading && e.key === ' ') {
+        console.log('hi')
+      }
+    }
+    document.addEventListener('keydown', handleKeyDown)
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [isLoading])
+
+  pusher.connection.bind('connected', () => {
+    setIsLoading(false)
+    pusher.subscribe('channel-1')
+  })
+
+  if (isLoading) return <div>Loading...</div>
+
+  
+  
   return (
     <PusherProvider pusher={pusher}>
-      <p>{JSON.stringify(pusher.config)}</p>
+      <p>Connected</p>
     </PusherProvider>
   )
 }
